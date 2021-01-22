6a4b6d861ff14d8ffa35581fa71b392389a4950e
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-01-22 16:48:16 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello to Task Tracker App</h1>
+      <p>Ready to start a new Project</p>
+    </div>
+  );
+}
+
+export default App;
