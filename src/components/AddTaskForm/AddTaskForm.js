1ef434a3a7b67cdd62bffcfcef3bafaa7f77a9bc
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-01-24 22:43:28 +0200

    added AddTaskForm a component with styles, made it responsive and additional functionality to add a new task and delete an existing one

--- src/components/AddTaskForm/AddTaskForm.js
+++ src/components/AddTaskForm/AddTaskForm.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import styles from './AddTaskForm.module.css'
-import { useState, useRef } from "react"
+import { useState } from "react"
 
 const AddTaskForm = ({ addTask }) => {
    const [text, setText] = useState(""); // Task
    const [date, setDate] = useState(""); // Date
    const [reminder, setReminder] = useState(false); // Specific Task Reminder
-   const someRef = useRef(null);
 
    const onSubmit = (event) => {
       event.preventDefault();
 
-      if (!text) {
-         alert("Plese enter your task")
-         console.log(someRef.current)
-         return;
-      }
+      // if (!text) {
+      //    alert("Plese enter your task")
+      //    console.log(someRef.current)
+      //    return;
+      // }
       addTask({ text, date, reminder });
 
       setText("");
@@ -26,7 +25,7 @@ const AddTaskForm = ({ addTask }) => {
       <form className={styles.form} onSubmit={onSubmit}>
          <div className={styles["form-item"]}>
             <label className={styles.label} htmlFor="usertask">New Task</label>
-            <input className={styles.input} ref={someRef} type="text" value={text} placeholder="Enter Your Task..." id="usertask" onChange={(event) => setText(event.target.value)} />
+            <input className={styles.input}  type="text" value={text} placeholder="Enter Your Task..." id="usertask" required onChange={(event) => setText(event.target.value)} />
          </div>
          <div className={styles["form-item"]}>
             <label className={styles.label} htmlFor="userdate">New Task</label>
